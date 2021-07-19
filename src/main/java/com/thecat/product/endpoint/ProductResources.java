f2050ee4be10babb1353abc50787e3798ef1320a
Felix Roberge <roberge.felix@gmail.com>
2021-07-19 18:44:27 -0400

    remove unecessary import

--- src/main/java/com/thecat/product/endpoint/ProductResources.java
+++ src/main/java/com/thecat/product/endpoint/ProductResources.java
@@ -9,8 +9,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import com.thecat.product.service.ProductService;
 import com.thecat.product.model.Product;
 
